363fe196b82c6777bf5971366701d639414a671f
mohitbharde <mohitbharde@gmail.com>
2025-08-14 22:29:54 +0530

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import VideoCall from "./components/videoCall";
+import VideoCall from "./components/VideoCall";
 import HomePage from "./components/HomePage";
 
 function App() {
